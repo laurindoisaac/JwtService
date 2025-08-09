e248c71d65897c56eea5f77603c6b705264859f0
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 01:23:13 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/jwtservice/main.go
@@ -0,0 +1,23 @@
+// cmd/jwtservice/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "jwtservice/internal/jwtservice"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := jwtservice.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
